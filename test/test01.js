b760b82a4b848d369ddba92a3dc38785ebd3be92
zlk <lingkangzhangjava@163.com>
2014-08-02 11:24:50 +0800

    添加测试哭

--- test/test01.js
+++ test/test01.js
@@ -3,24 +3,20 @@
  */
 "use strict";
 (function () {
-    var tpl = document.querySelector("#hello-tpl"),
-        container = document.querySelector("#container"),
-        data = {
-            name: "zlk",
-            age: 23,
-            persons: [
-                {
-                    name: "zlk"
-                },
-                {
-                    name: "zhuqian"
-                },
-                {
-                    name: "liuhcao"
-                }
-            ]
-        },
-        view = ky.template(tpl.text, data);
-    container.innerHTML = view;
+    mocha.setup('bdd');
+    chai.Should();
 
-})();
+    describe("ky", function () {
+        describe("template", function () {
+            it("if条件为true的时候，输出if语句块的内容", function () {
+                var tpl = document.querySelector("#if-true").text;
+                ky.template(tpl, null).trim().should.eql("if-true");
+            });
+            it("if条件为false的时候，输出else语句块的内容", function () {
+                var tpl = document.querySelector("#if-false").text;
+                ky.template(tpl, null).trim().should.eql("if-false");
+            });
+        });
+    });
+    mocha.run();
+}());
