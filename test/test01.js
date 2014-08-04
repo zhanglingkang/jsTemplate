76e560f100ef9dba4f78e03fd5a7de04884ea7ec
zhanglingkang <lingkangzhangjava@163.com>
2014-08-04 10:43:17 +0800

    添加测试用例

--- test/test01.js
+++ test/test01.js
@@ -16,6 +16,21 @@
                 var tpl = document.querySelector("#if-false").text;
                 ky.template(tpl, null).trim().should.eql("if-false");
             });
+            it("for循环测试", function () {
+                var tpl = document.querySelector("#for").text,
+                    view = ky.template(tpl, null).trim();
+                view.should.contain("0");
+                view.should.contain("5");
+                view.should.contain("9");
+            });
+            it("表达式内函数测试", function () {
+                var tpl = document.querySelector("#exp-fn").text,
+                    view = ky.template(tpl, {
+                        fn: function () {
+                            return "123";
+                        }}).trim();
+                view.should.eql("123");
+            });
         });
     });
     mocha.run();
