427e3e344a30d0a255dbff4154bd0462d225b60e
nealzhu <nealzhuqian@163.com>
2014-08-05 15:43:00 +0800

    1、添加 demo； 2、补充漏加的变量声明； 3、模板渲染函数体循环添加； 4、替换部分简单的正则为字符串拼接。

--- src/template-engine.js
+++ src/template-engine.js
@@ -1,7 +1,9 @@
 "use strict";
+
 var global = (function () {
     return this || (0, eval)("this");
 }());
+
 (function () {
     /**
      * 为了使用yuidoc生成文档。这里才声明成了class。
@@ -11,7 +13,16 @@ var global = (function () {
 
     };
 
+    /**
+     * 模版数据输出或者方法输出的匹配
+     * @type {RegExp}
+     */
     var EXPRESSION_REG = /<%=([^%>]+)%>/;
+
+    /**
+     * js 代码匹配，包含 EXPRESSION_REG 的情况在内
+     * @type {RegExp}
+     */
     var CODE_REG = /<%([^%>]+)%>/g;
 
     /**
@@ -23,8 +34,10 @@ var global = (function () {
     ky.template = function (tpl, data) {
         return ky.templateFn(tpl)(data);
     };
+
     /**
-     * @method templateFn 有些场景下，一份模板可能需要渲染多个视图，调用此函数，可以将解析后的模板缓存起来，防止重复解析。
+     * 有些场景下，一份模板可能需要渲染多个视图，调用此函数，可以将解析后的模板缓存起来，防止重复解析
+     * @method templateFn 模版缓存
      * @param tpl {String} 模板代码
      * @return {Function} 模板解析后的函数，需要一个参数模型数据 data {Object}，返回值为渲染后的视图
      * @example
@@ -36,40 +49,52 @@ var global = (function () {
      */
     ky.templateFn = function (tpl) {
         /**
-         * CODE_REG在tpl中的某一匹配结果
+         * CODE_REG 在 tpl 中的每一次配结果
          * @type {Array}
          */
         var matchResult;
+
         /**
-         * CODE_REG在tpl中的某一匹配部分
+         * CODE_REG 在 tpl 中的每一次配结果匹配到的表达式
          * @type {String}
          */
         var matchPart;
+
         /**
-         * 模板lastPos之前的代码已被解析。
+         * EXPRESSION_REG 在 tpl 中的某一匹配部分
+         * @type {Array}
+         */
+        var expressionMatchResult;
+
+        /**
+         * view.join("")为模板与数据结合后渲染成的视图
+         * @type {Array}
+         */
+        var view = [];
+
+        /**
+         * 通过 exec 匹配的当前位置(模板 lastPos 之前的代码已被解析)
          * @type {number}
          */
         var lastPos = 0;
+
         /**
          * 模板的长度
          * @type {Number}
          */
         var length = tpl.length;
+
         /**
-         * 放入eval中执行的代码。
-         * @type {string}
-         */
-        var evalCode = "";
-        /**
-         * EXPRESSION_REG在tpl中的某一匹配部分
-         * @type {Array}
-         */
-        var expressionMatchResult;
-        /**
-         * 放入eval中的执行的代码片段。
+         * 存储放入 eval 中的执行的代码的临时片段
          * @type {String}
          */
         var evalCodePiece;
+
+        /**
+         * 最终放入 eval 中执行的代码
+         * @type {string}
+         */
+        var evalCode = "";
 
         /**
          * 模板解析后的函数的函数体。
@@ -79,15 +104,23 @@ var global = (function () {
 
         while (true) {
             matchResult = CODE_REG.exec(tpl);
+
             if (matchResult == null) {
                 evalCodePiece = "view.push('{content}');".replace("{content}", tpl.substring(lastPos, length).replace(/\n/g, ""));
                 evalCode += evalCodePiece;
                 break;
             } else {
+                // 截取第一段 js 输出代码
                 matchPart = tpl.substring(matchResult.index, CODE_REG.lastIndex);
+
+                // 截取从上一个匹配位置到下一个匹配位置间的模板代码
                 evalCodePiece = "view.push('{content}');".replace("{content}", tpl.substring(lastPos, matchResult.index).replace(/\n/g, ""));
                 evalCode += evalCodePiece;
+
+                // 设置匹配结束位置
                 lastPos = CODE_REG.lastIndex;
+
+                // 匹配到输出表达式，将为其赋值的代码加到运行代码中
                 if (expressionMatchResult = matchPart.match(EXPRESSION_REG)) {
                     evalCodePiece = [
                         "var value;",
@@ -104,6 +137,7 @@ var global = (function () {
                     ].join("").replace(/\{expression\}/g, expressionMatchResult[1]);
                     evalCode += evalCodePiece;
                 } else {
+                    // 匹配到 js 逻辑代码，直接加到运行代码中
                     evalCode += matchPart.substring(2, matchPart.length - 2);
                 }
             }
@@ -111,16 +145,20 @@ var global = (function () {
 
         fnBody = [
             "var view=[],",
-            "_defineVarCode='',",
-            "_key;",
-            "for(_key in data){",
-            "_defineVarCode+='var key=data[\"key\"];'.replace(/key/g,_key);",
-            "}",
-            "eval(_defineVarCode);",
-            evalCode,
+                "_defineVarCode='',",
+                "_key;",
+            "data.forEach(function(data, index) {",
+                "for(_key in data) {",
+                    "_defineVarCode+='var '+_key+'=data[\"'+_key+'\"];';",
+                "}",
+                "eval(_defineVarCode);",
+                evalCode,
+            "});",
             "return view.join('');"
         ].join("");
+
         console.log(fnBody);
+
         return new Function("data", fnBody);
     };
 }());
